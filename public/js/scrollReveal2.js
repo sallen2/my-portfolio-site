02e0a5ac69fc7fff68f7ff90f2489f39af3acfab
Stephan Allen <sallen202009@gmail.com>
2018-09-07 20:30:45 -0400

    init commit

--- public/js/scrollReveal2.js
+++ public/js/scrollReveal2.js
@@ -21,6 +21,13 @@ sr.reveal('#project3 h2', {
 	mobile: true
 });
 
+sr.reveal('#project4 h2', { 
+	duration: 1000,
+	origin: "top",
+	distance: "550px",
+	mobile: true
+});
+
 sr.reveal('.discription', { 
 	duration: 1000,
 	origin: "bottom",
