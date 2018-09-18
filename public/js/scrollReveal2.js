b4963e66c88c35908f7a11ce54b346cb929f569d
Stephan Allen <sallen202009@gmail.com>
2018-09-18 02:19:43 -0400

    added link to trivia game

--- public/js/scrollReveal2.js
+++ public/js/scrollReveal2.js
@@ -35,6 +35,13 @@ sr.reveal('#project5 h2', {
 	mobile: true
 });
 
+sr.reveal('#project6 h2', { 
+	duration: 1000,
+	origin: "top",
+	distance: "550px",
+	mobile: true
+});
+
 sr.reveal('.discription', { 
 	duration: 1000,
 	origin: "bottom",
